57cc6ee4bf3e53ec17b98101b0fd652d1a564bae
2snickers2 <93041229+2snickers2@users.noreply.github.com>
2024-11-26 12:44:46 +0200

    Delete Genius directory

--- Genius/js/modal.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector('.backdrop')
-const modalButtonOpen = document.querySelector('.modal-btn-open')
-const modalButtonClose = document.querySelector('.modal-btn-close')
-
-const toggleModal = () => modal.classList.toggle('is-hidden')
-
-modalButtonOpen.addEventListener('click', toggleModal);
-modalButtonClose.addEventListener('click', toggleModal);
